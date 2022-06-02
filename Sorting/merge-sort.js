7e85ffa16c5e8de4b88134648252f7d230278636
Matt Villa <matthewvilla805@gmail.com>
2022-06-02 12:22:45 -0700

    Merge Sort completed

--- Sorting/merge-sort.js
+++ Sorting/merge-sort.js
@@ -1,10 +1,99 @@
-let testArray = [ 18, 23, 11, 2, 18, 7, 6, 14, 9, 11 ];
+function merge(arr, p, q, r) {
+    
+    //assumption is arr is sorted from p to q, and from q+1 to r
+    let n1 = q - p + 1;
+    console.log(n1);
+    let n2 = r - q; 
+    console.log(n2);
 
-function mergeSort(arr, l, m, r){
+    console.log(arr);
+    console.log(p);
+    console.log(q);
+    console.log(r);
 
-    let n1 = m - l + 1;
-    let n2 = r - m;
+    let L = [];
+    let R = [];
 
-    let leftArray = new Array(n1);
-    let rightArray = new Array(n2);
+    for (let i = 0; i < n1; i++) {
+        L[i] = arr[i + p];
+    }
+    console.log(L);
+
+    for (let j = 0; j < n2; j++) {
+        R[j] = arr[(j + q) + 1];
+    }
+    console.log(R);
+
+    let Lindex = 0;
+    let Rindex = 0;
+    let k = p;
+
+    while (k <= r) {
+
+        if (L[Lindex] <= R[Rindex]) {
+            arr[k] = L[Lindex];
+            Lindex++;
+        } else {
+            arr[k] = R[Rindex];
+            Rindex++;
+        }
+        k++;
+
+        if (Lindex === n1 || Rindex === n2) {
+            break;
+        }
+    }
+    console.log(Rindex);
+    console.log(Lindex);
+    console.log(k);
+
+    while (Rindex !== n2) {
+        arr[k] = R[Rindex];
+        Rindex++;
+        k++;
+    }
+
+    console.log(Rindex);
+    console.log(Lindex);
+    console.log(k);
+
+    while (Lindex !== n1) {
+        arr[k] = L[Lindex];
+        Lindex++;
+        k++;
+    }
+    console.log(arr);
 }
+
+function mergeSort(arr, p, r) {
+    
+    if (p >= r) {
+        return;
+    }
+    
+    let q = Math.floor((r + p) / 2);
+    console.log(q);
+    // if (arr.length % 2 !== 0){
+    //     q -= 1;
+    // }
+    console.log(q);
+
+    mergeSort(arr, p, q);
+    mergeSort(arr, (q + 1), r);
+    merge(arr, p, q, r);
+
+}
+
+let testArray = [2, 5, 4, 7, 1, 3, 2, 6];
+let testArray2 = [3, 41, 52, 26, 38, 57, 9, 49];
+let testArray3 = [ 24, 25, 6, 22, 4, 4, 20, 20, 19, 18, 11 ];
+
+mergeSort(testArray, 0, testArray.length - 1);
+console.log(testArray);
+
+mergeSort(testArray2, 0, testArray2.length - 1);
+console.log(testArray2);
+
+mergeSort(testArray3, 0, testArray3.length - 1);
+console.log(testArray3);
+
