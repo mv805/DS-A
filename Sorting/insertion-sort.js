33b1fc2dd50adebfd9b40318e1c6d1fc9c72d8ef
Matt Villa <matthewvilla805@gmail.com>
2022-06-01 21:07:19 -0700

    Added some updates to the insertion sort

--- Sorting/insertion-sort.js
+++ Sorting/insertion-sort.js
@@ -11,7 +11,7 @@ function insertionSort(nums) {
         /* Move elements of arr[0..i-1], that are 
         greater than key, to one position ahead 
         of their current position */
-        while (j >= 0 && nums[j] > key) {
+        while (j >= 0 && nums[j] > nums[i]) {
             nums[j + 1] = nums[j];
             j--;
         }
